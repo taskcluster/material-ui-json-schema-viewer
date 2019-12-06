484a23ba6516d181df2a1ef8ad9aee3bef6bf59e
JiwoonKim <diana0501@naver.com>
2019-12-06 21:28:40 +0900

    Create sample version of SchemaTable component
    Created a sample SchemaTable component using material-ui grid in
    order to test how the component would look like. A simple JSON schema
    example is statically embedded into the components for this purpose.
    The result is that the two panels have each of their own
    horizontal scroll, but < br /> tags must be embedded in the left
    panel in order to align related rows with the right panel.

--- src/components/SchemaTable/index.jsx
+++ src/components/SchemaTable/index.jsx
@@ -1,11 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types'; // eslint-disable-line no-unused-vars
+import MuiGrid from '@material-ui/core/Grid';
+import './index.css'; // temporary stylesheet for testing
 
 function SchemaTable() {
+  const leftRows = [
+    (
+      <div>
+        <p>[</p>
+        <br />
+      </div>
+    ),
+    (
+      <div>
+        <p>&quot;...&quot;</p>
+        <br />
+        <br />
+      </div>
+    ),
+    (
+      <div>
+        <p>]</p>
+      </div>
+    ),
+  ];
+  const rightRows = [
+    (
+      <div>
+        <p>uniqueItems: true</p>
+        <p>additionalItems: false</p>
+      </div>
+    ),
+    (
+      <div>
+        <p>(string)</p>
+        <p>format: email</p>
+        <p>maxLength: 20</p>
+      </div>
+    ),
+    (<div><br /></div>),
+  ];
   return (
-    <div>
-      SchemaTable
-    </div>
+    <MuiGrid container wrap="nowrap">
+      <MuiGrid item className="left-panel">
+        {leftRows}
+      </MuiGrid>
+      <MuiGrid item className="right-panel">
+        {rightRows}
+      </MuiGrid>
+    </MuiGrid>
   );
 }
 
