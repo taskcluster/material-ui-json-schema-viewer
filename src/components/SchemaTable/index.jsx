a483737f07b62810252648b63c07da1887a0eef7
JiwoonKim <diana0501@naver.com>
2019-12-10 20:27:01 +0900

    Add style normalization and refactor schema props
    - Added material-ui's css baseline to nomalize styles
    - Refactored json schema as prop instead of statically using exampleSchema

--- src/components/SchemaTable/index.jsx
+++ src/components/SchemaTable/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types'; // eslint-disable-line no-unused-vars
+import { shape, string } from 'prop-types';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
 import NormalLeftRow from '../NormalLeftRow';
 import NormalRightRow from '../NormalRightRow';
@@ -9,7 +10,6 @@ const useStyles = makeStyles({
     display: 'grid',
     // TODO: change diminsions to fixed width size
     gridTemplateColumns: '[left-panel] 1fr [right-panel] 1fr',
-    rowGap: 4,
   },
   leftPanel: {
     // TODO: accept theme
@@ -20,9 +20,7 @@ const useStyles = makeStyles({
     overflowX: 'auto',
   },
   row: {
-    boxSizing: 'border-box',
-    margin: 0,
-    padding: 0,
+    borderBottom: '1px black solid',
   },
   rightRow: {
     display: 'grid',
@@ -35,14 +33,14 @@ const useStyles = makeStyles({
 
   },
   line: {
+    boxSizing: 'border-box',
+    margin: 0,
+    padding: 0,
     whiteSpace: 'nowrap',
   },
 });
 
-// TODO: eliminate after testing
-const exampleSchema = require('../../../schemas/basicDataTypes/string/stringPattern.json');
-
-function SchemaTable() {
+function SchemaTable({ schema }) {
   const classes = useStyles();
 
   /**
@@ -58,35 +56,31 @@ function SchemaTable() {
    * TODO: define renderArray method
    *       add description comment
    */
-  const renderArray = (schema) => {
-
-  };
+  const renderArray = (schemaInput) => <>{schemaInput}</>;
 
   /**
    * TODO: define renderCombination method
    *       add description comment
    */
-  const renderCombination = (schema) => {
-
-  };
+  const renderCombination = (schemaInput) => <>{schemaInput}</>;
 
   /**
    * Default data type schemas (boolean, null, number, integer, string)
    * are parsed to create 'NormalLeftRow' and 'NormalRightRow' components.
    * These will then be stored into 'leftRows' and 'rightRows' arrays respectively.
    */
-  const renderDefault = (schema) => {
+  const renderDefault = (schemaInput) => {
     const leftRow = (
       <NormalLeftRow
         key={leftRows.length + 1}
-        schema={schema}
+        schema={schemaInput}
         classes={classes}
       />
     );
     const rightRow = (
       <NormalRightRow
         key={rightRows.length + 1}
-        schema={schema}
+        schema={schemaInput}
         classes={classes}
       />
     );
@@ -99,67 +93,67 @@ function SchemaTable() {
    * TODO: define renderObject method
    *       add description comment
    */
-  const renderObject = (schema) => {
-
-  };
+  const renderObject = (schemaInput) => <>{schemaInput}</>;
 
   /**
    * TODO: define renderRef method
    *       add description comment
    */
-  const renderRef = (schema) => {
-
-  };
+  const renderRef = (schemaInput) => <>{schemaInput}</>;
 
   /**
    * Schemas are passed to different render methods according to its
    * specific type (combination, array, object, ref, and default).
    * Each method will create rows with the appropriate format to its
    * type and push the rows into 'leftRows' and 'rightRows' respectively.
-   *
+   * The rows will then be rendered within the left and right panels.
    * Types other than default schemas may repeatedly call this method
    * within their render method if the schema has a nested structure.
    */
-  const renderSchema = (schema) => {
-    const combinationTypes = ['allOf', 'anyOf', 'oneOf', 'not'];
-    const defaultTypes = ['boolean', 'null', 'number', 'integer', 'string'];
-
-    combinationTypes.forEach((keyword) => {
-      if (keyword in schema) {
-        renderCombination(schema);
-      }
-    });
-    if (schema.type === 'array') {
-      renderArray(schema);
-    } else if (schema.type === 'object') {
-      renderObject(schema);
-    } else if (defaultTypes.includes(schema.type)) {
-      renderDefault(schema);
+  const renderSchema = (schemaInput) => {
+    if ('allOf' in schemaInput || 'anyOf' in schemaInput || 'oneOf' in schemaInput || 'not' in schemaInput) {
+      renderCombination(schemaInput);
+    } else if ('$ref' in schemaInput) {
+      renderRef(schemaInput);
+    } else if (schemaInput.type === 'array') {
+      renderArray(schemaInput);
+    } else if (schemaInput.type === 'object') {
+      renderObject(schemaInput);
     } else {
-      // TODO: handle exception cases of empty JSON schemas
+      renderDefault(schemaInput);
     }
+    // TODO: handle empty json schemas
+
+    return (
+      <div className={classes.wrapper}>
+        <div className={classes.leftPanel}>
+          {leftRows}
+        </div>
+        <div className={classes.rightPanel}>
+          {rightRows}
+        </div>
+      </div>
+    );
   };
 
-  renderSchema(exampleSchema);
-
   return (
-    <div className={classes.wrapper}>
-      <div className={classes.leftPanel}>
-        {leftRows}
-      </div>
-      <div className={classes.rightPanel}>
-        {rightRows}
-      </div>
-    </div>
+    <>
+      <CssBaseline />
+      {renderSchema(schema)}
+    </>
   );
 }
 
 SchemaTable.propTypes = {
-
+  schema: shape({
+    type: string,
+  }),
 };
 
 SchemaTable.defaultProps = {
-
+  schema: {
+    type: 'null',
+  },
 };
 
 export default SchemaTable;
