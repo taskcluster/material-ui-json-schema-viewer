31c529d10869b213967fff55707ed1cc51cc7ed8
JiwoonKim <diana0501@naver.com>
2019-12-17 16:05:10 +0900

    Add comments for props for SchemaTable, NormalRows
    Added comments for the following components
    - SchemaTable
    - NormalLeftRow
    - NormalRightRow

--- src/components/NormalLeftRow/index.jsx
+++ src/components/NormalLeftRow/index.jsx
@@ -43,11 +43,22 @@ function NormalLeftRow({ schema, classes }) {
 }
 
 NormalLeftRow.propTypes = {
+  /** 
+   * Schema input given to render.
+   * May also be a sub-schema in case for array items,
+   * object properties or more complex schemas. 
+  */
   schema: shape({
-    type: string,
+    /** Type of schema or sub-schema */
+    type: string.isRequired,
+    /** Name of schema or sub-schema */
     name: string,
-    close: string,
   }).isRequired,
+  /**
+   * Style for rows and lines for schema viewer.
+   * Necessary to maintain consistency with right panel's
+   * rows and lines.
+   */
   classes: shape({
     row: string.isRequired,
     line: string.isRequired,
--- src/components/NormalRightRow/index.jsx
+++ src/components/NormalRightRow/index.jsx
@@ -33,15 +33,32 @@ function NormalRightRow({ schema, classes }) {
 }
 
 NormalRightRow.propTypes = {
+  /** 
+   * Schema input given to render.
+   * May also be a sub-schema in case for array items,
+   * object properties or more complex schemas. 
+  */
   schema: shape({
+    /** Type of schema or sub-schema */
     type: string.isRequired,
   }).isRequired,
+  /**
+   * Style for rows and lines for schema viewer.
+   * Necessary to maintain consistency with right panel's
+   * rows and lines.
+   */
   classes: shape({
     row: string.isRequired,
-    rightRow: string.isRequired,
-    keywordColumn: string.isRequired,
-    descriptionColumn: string.isRequired,
     line: string.isRequired,
+    /** 
+     * Display the right panel in a two-column grid 
+     * : keywordColumn, descriptionColumn
+     */
+    rightRow: string.isRequired,
+    /** Column to display keywords for the schema or sub-schema */
+    keywordColumn: string.isRequired,
+    /** Column to display description for the schema or sub-schema */
+    descriptionColumn: string.isRequired,
   }).isRequired,
 };
 
