136b9f417f0b0d6fa95bd7ae93f1ef92ac1dc8fb
Jiwoon Kim <diana0501@naver.com>
2019-12-20 15:06:51 +0900

    Revert "Create renderArray() method"

--- src/components/NormalLeftRow/index.jsx
+++ src/components/NormalLeftRow/index.jsx
@@ -1,80 +1,47 @@
 import React from 'react';
 import { shape, string } from 'prop-types';
-import Typography from '@material-ui/core/Typography';
 
 function NormalLeftRow({ schema, classes }) {
-  /**
-   * If schema or sub-schema has a name keyword,
-   * (ex. properties of object) store in variable to display.
+  const name = 'name' in schema ? `${schema.name}: ` : null;
+  const typeSymbol =
+    schema.type === 'array' || schema.type === 'object' ? (
+      {
+        array: '[',
+        object: '{',
+      }[schema.type]
+    ) : (
+      <code className={classes.code}>{schema.type}</code>
+    );
+  /** Create blank line paddings only for additional keywords that
+   *  will have their own lines on the according right row.
+   *  This enables the left row to have matching number of lines with
+   *  the right row and align the lines and heights between the two rows.
    */
-  const name = 'name' in schema ? schema.name : null;
-  /** Map schema's type to a symbol for display. */
-  const typeSymbol = {
-    array: '[',
-    boolean: '"..."',
-    integer: '"..."',
-    null: '"..."',
-    number: '"..."',
-    object: '{',
-    string: '"..."',
-  }[schema.type];
-  /**
-   * Create blank line paddings only for additional keywords that
-   * will have their own lines on the according right row.
-   * This enables the left row to have matching number of lines with
-   * the right row and align the lines and heights between the two rows.
-   */
-  const nonPaddedKeywords = [
-    'type',
-    'description',
-    'name',
-    'items',
-    'contains',
-  ];
+  const nonPaddedKeywords = ['type', 'description', 'name'];
   const blankLinePaddings = [];
 
   Object.keys(schema).forEach(keyword => {
     if (!nonPaddedKeywords.includes(keyword)) {
-      blankLinePaddings.push(
-        <Typography
-          key={`${keyword} line`}
-          component="div"
-          variant="subtitle2"
-          className={classes.line}>
-          <br />
-        </Typography>
-      );
+      blankLinePaddings.push(<br key={keyword} className={classes.line} />);
     }
   });
 
   return (
-    <div key={schema.type} className={classes.row}>
-      <Typography component="div" variant="subtitle2" className={classes.line}>
-        {name && `${name}: `}
+    <div className={classes.row}>
+      <p className={classes.line}>
+        {name}
         {typeSymbol}
-      </Typography>
+      </p>
       {blankLinePaddings}
     </div>
   );
 }
 
 NormalLeftRow.propTypes = {
-  /**
-   * Schema input given to render.
-   * May also be a sub-schema in case for array items,
-   * object properties or more complex schemas.
-   */
   schema: shape({
-    /** Type of schema or sub-schema */
     type: string.isRequired,
-    /** Name of schema or sub-schema */
     name: string,
   }).isRequired,
-  /**
-   * Style for rows and lines for schema viewer.
-   * Necessary to maintain consistency with right panel's
-   * rows and lines.
-   */
   classes: shape({
     row: string.isRequired,
     line: string.isRequired,
--- src/components/NormalRightRow/index.jsx
+++ src/components/NormalRightRow/index.jsx
@@ -1,73 +1,37 @@
 import React from 'react';
 import { shape, string } from 'prop-types';
-import Typography from '@material-ui/core/Typography';
 
 function NormalRightRow({ schema, classes }) {
   const keywords = Object.keys(schema);
-  const nonDisplayedKeywords = ['items', 'contains'];
-
-  // TODO: specify details to see below?
-  /*
-  if (typeof schema.additionalItems === 'object') {
-  }
-  */
 
   return (
     <div className={`${classes.row} ${classes.rightRow}`}>
       <div className={classes.keywordColumn}>
-        {keywords.map(
-          keyword =>
-            !nonDisplayedKeywords.includes(keyword) && (
-              <Typography
-                key={keyword}
-                component="div"
-                variant="subtitle2"
-                className={classes.line}>
-                {keyword}
-                {': '}
-                {schema[keyword]}
-              </Typography>
-            )
-        )}
+        {keywords.map(keyword => (
+          <p key={keyword} className={classes.line}>
+            {keyword}
+            {': '}
+            {schema[keyword]}
+          </p>
+        ))}
       </div>
       <div className={classes.descriptionColumn}>
-        {'description' in schema && (
-          <Typography component="div" variant="subtitle2">
-            {schema.description}
-          </Typography>
-        )}
+        {'description' in schema && <p>{schema.description}</p>}
       </div>
     </div>
   );
 }
 
 NormalRightRow.propTypes = {
-  /**
-   * Schema input given to render.
-   * May also be a sub-schema in case for array items,
-   * object properties or more complex schemas.
-   */
   schema: shape({
-    /** Type of schema or sub-schema */
     type: string.isRequired,
   }).isRequired,
-  /**
-   * Style for rows and lines for schema viewer.
-   * Necessary to maintain consistency with right panel's
-   * rows and lines.
-   */
   classes: shape({
     row: string.isRequired,
-    line: string.isRequired,
-    /**
-     * Display the right panel in a two-column grid
-     * : keywordColumn, descriptionColumn
-     */
     rightRow: string.isRequired,
-    /** Column to display keywords for the schema or sub-schema */
     keywordColumn: string.isRequired,
-    /** Column to display description for the schema or sub-schema */
     descriptionColumn: string.isRequired,
+    line: string.isRequired,
   }).isRequired,
 };
 
