b2a21a033c0fe8ec503fd2fe78c7b71fe9e122d9
JiwoonKim <diana0501@naver.com>
2020-02-20 13:55:14 +0900

    Create custom Chip component with consistent chip style

---
+++ src/components/Chip/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shape, string, node, element } from 'prop-types';
+import MuiChip from '@material-ui/core/Chip';
+
+function Chip({ classes, label, icon }) {
+  return (
+    <MuiChip
+      className={classes.chip}
+      label={label}
+      icon={icon}
+      size="small"
+      variant="outlined"
+    />
+  );
+}
+
+Chip.propTypes = {
+  /**
+   * Style for chips used in schema table.
+   */
+  classes: shape({
+    chip: string.isRequired,
+  }).isRequired,
+  label: node.isRequired,
+  icon: element,
+};
+
+Chip.defaultProps = {
+  icon: null,
+}
+
+export default React.memo(Chip);
