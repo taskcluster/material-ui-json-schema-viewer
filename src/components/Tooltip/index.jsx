2c80187e6877d9d3e6b53613edb4ad607f0a0d43
JiwoonKim <diana0501@naver.com>
2020-02-20 13:51:56 +0900

    Add forwardRef feature in Tooltip component
    Add forwardRef to children components in Tooltip so that the tooltip
    applies to even custom components. The result is that children nodes of
    the Tooltip component are wrapped within a div and are passed properties
    and ref of the material-ui's Tooltip so that a tooltip can apply to even
    custom created components.

--- src/components/Tooltip/index.jsx
+++ src/components/Tooltip/index.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import { node } from 'prop-types';
 import MuiTooltip from '@material-ui/core/Tooltip';
 
+/**
+ * Tooltip accessible by hovering mouse or touching mobile screen.
+ */
 function Tooltip({ title, children }) {
+  /**
+   * Forward the properties and ref of the tooltip to the children components
+   * so that MuiTooltip has control over the wrapped components' props.
+   */
+  const ChildrenWrapped = React.forwardRef(function MyComponent(props, ref) {
+    return <div {...props} ref={ref}>{children}</div>
+  });
+
   return (
     <MuiTooltip title={title} arrow disableFocusListener enterTouchDelay={1}>
-      {children}
+      <ChildrenWrapped />
     </MuiTooltip>
   );
 }
