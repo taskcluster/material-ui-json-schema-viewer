0cbe902300e743088bec77a3bff7ae7346459812
Dustin J. Mitchell <dustin@mozilla.com>
2020-09-18 12:40:53 -0400

    refactor RightCell to remove 'lines' concept

--- src/components/SchemaTable/RightCell.jsx
+++ src/components/SchemaTable/RightCell.jsx
@@ -8,11 +8,7 @@ import Markdown from '../Markdown';
 import Chip from '../Chip';
 import Tooltip from '../Tooltip';
 import { basicTreeNode } from '../../utils/prop-types';
-import {
-  SKIP_KEYWORDS,
-  MAX_NUMBER_OF_CHIPS,
-  TOOLTIP_DESCRIPTIONS,
-} from '../../utils/constants';
+import { SKIP_KEYWORDS, TOOLTIP_DESCRIPTIONS } from '../../utils/constants';
 
 const useStyles = makeStyles(theme => ({
   typography: {
@@ -88,103 +84,49 @@ function RightCell({ treeNode }) {
   }
 
   /**
-   * Create lines for specification keywords in groups of chips.
-   * Each specification keyword is displayed as a chip and
-   * a single line may only contain a MAX_NUMBER_OF_CHIPS at most.
+   * Each specification keyword is displayed as a chip.
    */
-  function createSpecificationLines(specKeywords) {
-    const lines = [];
-    /**
-     * Sort specification keywords in alphabetical order and
-     * group the keywords in sizes of MAX_NUMBER_OF_CHIPS.
-     */
-    const keywordGroups = [[]];
-
-    specKeywords.sort();
-    specKeywords.forEach((keyword, i) => {
-      const groupIndex = Math.trunc(i / MAX_NUMBER_OF_CHIPS);
-
-      if (groupIndex > keywordGroups.length - 1) {
-        keywordGroups.push([]);
-      }
-
-      keywordGroups[groupIndex].push(keyword);
-    });
-
-    /**
-     * Each keyword group displays a single line containing
-     * a MAX_NUMBER_OF_CHIPS amount of chips
-     */
-    keywordGroups.forEach(keywordGroup => {
-      lines.push(
-        <div
-          key={`spec-line-${keywordGroup.toString()}`}
-          className={classes.typography}>
-          {keywordGroup.map(keyword => createKeywordChip(keyword))}
-        </div>
-      );
-    });
-
-    return lines;
+  function createKeywordChips() {
+    return (
+      <div className={classes.typography}>
+        {specKeywords.map(keyword => createKeywordChip(keyword))}
+      </div>
+    );
   }
 
   /**
    * Display the title keyword in a single line.
    */
-  function createTitleLine(keyword) {
+  function createTitle() {
     return (
-      <Typography
-        className={classes.typography}
-        variant="subtitle2"
-        key={keyword}>
-        <strong>{schema[keyword]}</strong>
-      </Typography>
+      schema.title && (
+        <Typography className={classes.typography} variant="subtitle2">
+          <strong>{schema.title}</strong>
+        </Typography>
+      )
     );
   }
 
   /**
    * Display the descriptive keyword in a single line.
    */
-  function createDescriptionLine(keyword) {
+  function createDescription() {
     return (
-      <Typography className={classes.typography} variant="body2" key={keyword}>
-        <Markdown>{schema[keyword]}</Markdown>
-      </Typography>
+      schema.description && (
+        <Typography className={classes.typography} variant="body2">
+          <Markdown>{schema.description}</Markdown>
+        </Typography>
+      )
     );
   }
 
-  /**
-   * Create lines for the schema's specification and descriptive keywords.
-   * @param {array} specKeywords specification keywords in schema
-   * @returns {array} lines for the schema's right panel
-   */
-  function createLinesForKeywords(specKeywords) {
-    let lines = [];
-
-    /**
-     * If descriptive keywords exist, display each keyword in its own line.
-     */
-    if ('title' in schema) {
-      lines.push(createTitleLine('title'));
-    }
-
-    if ('description' in schema) {
-      lines.push(createDescriptionLine('description'));
-    }
-
-    /**
-     * If specification keywords exist, create lines to hold keyword chips.
-     */
-    if (specKeywords.length > 0) {
-      const specLines = createSpecificationLines(specKeywords);
-
-      lines = lines.concat(specLines);
-    }
-
-    return lines;
-  }
-
-  return <div>{createLinesForKeywords(specKeywords)}</div>;
+  return (
+    <div>
+      {createTitle()}
+      {createDescription()}
+      {specKeywords.length > 0 && createKeywordChips()}
+    </div>
+  );
 }
 
 RightCell.propTypes = {
